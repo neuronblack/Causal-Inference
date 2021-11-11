cdf7c62029a237a15d5a08146ade74f50ff05f76
geshenyang <geshenyang@xiaoice.com>
2021-11-11 13:55:16 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -292,7 +292,7 @@ texinfo_documents = [
 
 # app setup hook
 def setup(app):
-    app.add_config_value('causalinference_config', {
+    app.add_config_value('recommonmark_config', {
         #'url_resolver': lambda url: github_doc_root + url,
         'auto_toc_tree_section': 'Contents',
         'enable_math': False,
